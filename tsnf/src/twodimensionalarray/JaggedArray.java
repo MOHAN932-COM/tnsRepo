061cf1e57dde66f607e1d4b38f6c7990ff2327cc
MOHAN932-COM-code <mrnandan678@gmail.com>
2025-09-23 20:31:42 +0530

    Merge branch 'master' of https://github.com/MOHAN932-COM/tnsRepo.git

---
+++ tsnf/src/twodimensionalarray/JaggedArray.java
@@ -0,0 +1,19 @@
+package twodimensionalarray;
+
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		int[][] jagged = {
+				{1,2},
+				{3,4,5},
+				{6}
+		};
+		for(int i = 0; i < jagged.length; i++) {
+			for(int j = 0; j < jagged[i].length; j++) {
+				System.out.print(jagged[i][j] + " ");
+			}
+			System.out.println();
+		}
+}
+
+}
